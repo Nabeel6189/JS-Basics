16b339c1bae9e939a283d9fdf0645d3a81e39dee
Nabeel6189 <nabeel13b@gmail.com>
2022-12-04 12:20:15 +0530

    Added a case in the scopeChain

--- jsCourseModules/scopeChain.js
+++ jsCourseModules/scopeChain.js
@@ -4,7 +4,7 @@ var names = 'nabeel';
 username();
 
 function username (){
-    var names = 5;
+    var names = 4;
     console.log(names);
     newUser();
 
@@ -23,16 +23,23 @@ console.log(names);
 
 username1();
 
+var newNames = "drake";
+
 function username1 (){
-    var names1 = 5;
-    console.log(names1);
+    var newNames = 5;
+    console.log(newNames);
     newUser1();
 
     //This functions will take the value from the outer function, if the outer function does not have a variable, then it will search globally
     function newUser1 (){
-        console.log(names1);
+        console.log(newNames);
     }
 }
 
 //Here the console log does not take the value from the function and errors out
-console.log(names1);
+console.log(newNames);
+
+// const realConfig = realConfig("index.html", (err, data) => {
+
+//  console.log('this is true');
+// });
